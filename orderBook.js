a842044a03060ab9a9a15eee3eaafcb9c38716fe
Cat <catb0591@gmail.com>
2021-11-16 19:37:51 -0500

    Final Answer

--- orderBook.js
+++ orderBook.js
@@ -1,20 +1,34 @@
 const reconcileOrder = (existingBook, incomingOrder) => {
-    const orderBook = existingBook
-    
-    orderBook.push(incomingOrder)
-    
-        return orderBook
+  const newBook = []
+  let newTransaction = incomingOrder
+
+  for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].price === incomingOrder.price && existingBook[i].type !== incomingOrder.type) {
+      newTransaction = transactionToLog(existingBook[i], newTransaction)
+    } else if (existingBook[i].price > incomingOrder.price && existingBook[i].type !== incomingOrder.type &&
+              existingBook[i].type === 'buy') {
+      newTransaction = transactionToLog(existingBook[i], newTransaction)
+    } else { newBook.push(existingBook[i]) }
+  }
+  if (newTransaction !== null) {
+    newBook.push(newTransaction)
+  }
+
+  return newBook
 }
+
+const transactionToLog = (existingBook, newTransaction) => {
+  if ((existingBook.quantity) - (newTransaction.quantity) === 0) {
+    return null
+  }
+  if ((existingBook.quantity) - (newTransaction.quantity) < 0) {
+    const newQuantity = (newTransaction.quantity) - (existingBook.quantity)
+
+    return { ...existingBook, quantity: newQuantity, type: newTransaction.type }
+  }
+  const newQuantity = (existingBook.quantity) - (newTransaction.quantity)
+
+  return { ...existingBook, quantity: newQuantity }
+}
+
 module.exports = reconcileOrder
-
-//try for loops to increase the sell amounts and the buy amounts. Then add or subtract 
-
-//Have to take into account the quantity and the price. Can you subtract from quantity if value equal amount of new qu
-
-//Book is an array with objects as index?
-
-// book1 = []
-// book2 = { type: 'sell', quantity: 10, price: 6150 },{ type: 'sell', quantity: 12, price: 6000 }
-
-// console.log (reconcileOrder(book1, book2))
-//for existing 
